54fa92347a0edaf48d8568efee20cb907c3d3096
Szymon Andrzejewski <hello@szymonandrzejewski.pl>
2024-12-20 22:33:59 +0100

    build

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 rmdir /s/q build
 rmdir /s/q dist
-pyinstaller --noconfirm --onefile --console --icon "icon.ico" --name "spotify-scheduler" --add-data "icon.ico;." "main.py"
+pyinstaller --noconfirm --onefile --console --icon "icon.ico" --name "spotify-scheduler" --add-data "icon.ico;." --hidden-import='PIL._tkinter_finder' "main.py"
 pause
