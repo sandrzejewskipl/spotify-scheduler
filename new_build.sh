cd95c798b1e617a8c16592e477027c81f574fad0
Szymon Andrzejewski <hello@szymonandrzejewski.pl>
2024-12-20 23:08:06 +0100

    Update new_build.sh

--- new_build.sh
+++ new_build.sh
@@ -1,2 +1,5 @@
+mkdir output
 docker run --volume "$(pwd):/src/" --entrypoint /bin/sh batonogov/pyinstaller-linux:main-slim-bullseye -c "apt update -y && apt-get install python3-tk -y && /entrypoint.sh"
-#docker run --volume "$(pwd):/src/" batonogov/pyinstaller-windows:latest
+mv dist/* output/
+docker run --volume "$(pwd):/src/" batonogov/pyinstaller-windows:latest
+mv dist/* output/
