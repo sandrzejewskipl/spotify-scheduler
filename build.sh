4214e1f3f52a0eaae138312f3f28e95bab73d8ce
Szymon Andrzejewski <hello@szymonandrzejewski.pl>
2024-12-20 20:18:20 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 python3 -m venv .venv
 source .venv/bin/activate
-python3 -m pip install -r requirements.
+python3 -m pip install -r requirements.txt
 python3 -m pip install pyinstaller
 pyinstaller --noconfirm --onefile --console --icon "icon.ico" --name "Spotify Scheduler" --add-data "icon.ico:." "main.py"
