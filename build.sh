2877d6cd5113916c32838638600fc2fe521caa81
Szymon Andrzejewski <hello@szymonandrzejewski.pl>
2024-12-20 20:03:43 +0100

    build

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
+python3 -m venv .venv
+source .venv/bin/activate
+python3 -m pip install -r requirements.txt
 pyinstaller --noconfirm --onefile --console --icon "icon.ico" --name "Spotify Scheduler" --add-data "icon.ico:." "main.py"
