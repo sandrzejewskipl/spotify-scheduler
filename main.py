1826e6424aa853acdae469ed85ccd645f741dba1
Szymon Andrzejewski <hello@szymonandrzejewski.pl>
2024-12-25 17:42:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1247,7 +1247,7 @@ def spotify_main():
 def main():
     global CLIENT_ID, CLIENT_SECRET, config
     if(not CLIENT_ID or not CLIENT_SECRET):
-        print(f"Create an app here (instructions are in README on Github):\nhttps://developer.spotify.com/dashboard (should open automatically)")
+        print(f"Create an app here (instructions are in README on Github):\nhttps://developer.spotify.com/dashboard (it should open automatically)")
         t.sleep(1.5)
         open_link("https://developer.spotify.com/dashboard")
         CLIENT_ID = input("Enter CLIENT_ID: ")
