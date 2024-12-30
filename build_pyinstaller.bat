5b3d64b2224c2499992837e4390a296fbf3abe02
Szymon Andrzejewski <hello@szymonandrzejewski.pl>
2024-12-30 10:00:03 +0100

    Update build_pyinstaller.bat

--- build_pyinstaller.bat
+++ build_pyinstaller.bat
@@ -1,5 +1,6 @@
 @echo off
+set /p "id=Enter Version: "
 rmdir /s/q build
 rmdir /s/q dist
-pyinstaller --noconfirm --onefile --windowed --icon "icon.ico" --name "spotify-scheduler" --add-data "icon.ico;." "main.py"
+pyinstaller --noconfirm --onefile --windowed --icon "icon.ico" --name "spotify-scheduler" -n "spotify-scheduler-v%id%.exe" --add-data "icon.ico;." "main.py"
 pause
