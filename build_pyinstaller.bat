2f8a653cd58038a56200a0e91b57ce0a86ff9902
Szymon Andrzejewski <hello@szymonandrzejewski.pl>
2025-01-03 23:19:34 +0100

    file name change

--- build_pyinstaller.bat
+++ build_pyinstaller.bat
@@ -1,5 +1,5 @@
 @echo off
 rmdir /s/q build
 rmdir /s/q dist
-pyinstaller --noconfirm --onefile --windowed --icon "icon.ico" --name "spotify-scheduler" --add-data "icon.ico;." "main.py"
+pyinstaller --noconfirm --onedir --windowed --icon "icon.ico" --name "spotify-scheduler" --add-data "icon.ico;." "spotifyscheduler.py"
 pause
